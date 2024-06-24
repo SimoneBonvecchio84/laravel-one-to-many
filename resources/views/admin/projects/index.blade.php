5f25a4dd570b8bf2ce736b0b2e15b638d6af33d7
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-06-24 18:32:38 +0200

    Feat: add colum table to index.blade. Beetwen tag td after curProject->title added  curProject->type->name

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -16,6 +16,7 @@
             <tr>
                 <th scope="col">ID</th>
                 <th scope="col">Titolo</th>
+                <th scope="col">Categoria</th>
                 <th scope="col">Contenuto</th>
                 <th scope="col">Slug</th>
                 <th scope="col">Dettagli</th>
@@ -28,6 +29,7 @@
                 <tr>
                     <th scope="row">{{ $curProject->id }}</th>
                     <td>{{ $curProject->title }}</td>
+                    <td>{{ $curProject->type->name }}</td>
                     <td>{{ $curProject->content }}</td>
                     <td>{{ $curProject->slug }}</td>
                     <td>
